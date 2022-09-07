2639c6c9d7cc35040f689a01aeca38fb810335bb
AsiriSanda <sadakelum1234@gmail.com>
2022-09-07 23:31:10 +0530

    post updated

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const user = require('./routes/User')
+const post = require('./routes/Post')
 const app = express()
 const port = 4000
 
 app.use(express.json())
 app.use('/user', user)
+app.use('/post', post)
 
 const url = 'mongodb://localhost/fbClone'
 mongoose.connect(url,{useNewUrlParser: true})
